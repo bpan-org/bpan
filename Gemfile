7f69d0ec8220e89cf664a7e47131fcd017174dd0
aocole <aocole@gmail.com>
2014-04-10 15:01:17 +0800

    Star->authors.json working

--- Gemfile
+++ Gemfile
@@ -2,4 +2,4 @@
 source "https://rubygems.org"
 
 gem 'sinatra'
-
+gem 'git'
--- bpan.rb
+++ bpan.rb
@@ -1,3 +1,4 @@
+Bundler.require
 require 'sinatra'
 require 'json'
 require 'logger'
@@ -5,10 +6,17 @@ require 'logger'
 $stdout.sync = true
 logger = Logger.new $stdout
 
+INDEX_DIR = File.join(File.dirname(__FILE__), 'index')
+INDEX_BRANCH = 'index'
+AUTHORS_FILE = File.join(INDEX_DIR, 'authors.json')
+
 post '/star/?' do
   request.body.rewind  # in case someone already read it
   data = JSON.parse request.body.read
-  logger.debug data.inspect
+  sender = data['sender']['login']
+  logger.debug "Received star from #{sender.inspect}"
+  add_author sender
+  "Thanks for starring, #{sender}"
 end
 
 post '/push/?' do
@@ -16,11 +24,44 @@ post '/push/?' do
 end
 
 get '/?' do
-  'Welcome to BPAN'
+  redirect to('http://bpan.org')
 end
 
-def update
-  #packages.json
-  #authors.json
-  #README  
+private
+
+def ensure_index_dir
+  return if Dir.exist?(INDEX_DIR)
+  require 'fileutils'
+  FileUtils.mkdir_p(INDEX_DIR)
+  Git.clone('git@github.com:bpan-org/bpan-org.git', 'index', path: File.dirname(__FILE__), log: logger)
 end
+
+def repo
+  g = Git.open INDEX_DIR, log: logger
+  g.branch(INDEX_BRANCH).checkout
+  g.config('user.name', 'BPAN index')
+  g.config('user.email', 'index@bpan.org')
+  return g
+end
+
+def ensure_index_updated
+  ensure_index_dir
+  repo.pull 'origin', INDEX_BRANCH
+  return repo
+end
+
+def add_author author
+  git = ensure_index_updated
+  authors = JSON.parse(File.read(AUTHORS_FILE))
+  authors << author
+  authors.uniq!
+  authors.sort!
+  File.open(AUTHORS_FILE, 'w') {|f|
+    f.write authors.to_json
+  }
+  git.add(File.basename(AUTHORS_FILE))
+  git.commit("Add author '#{author}'")
+  git.push 'origin', INDEX_BRANCH
+end
+
+
