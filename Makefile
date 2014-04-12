55f65351293480d85a9cb72f71f554b6155b3422
aocole <aocole@gmail.com>
2014-04-12 11:30:31 +0800

    render gh-pages index.html

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
 SERVER=webhook.bpan.org
 EXCLUDE=--exclude-from=.gitignore --exclude=.git*
 
+.PHONY: ssh deploy tail
+
 deploy: 
 	rsync -avzL $(EXCLUDE) --include=id_rsa_server ./ $(SERVER):bpan-org/
-	ssh $(SERVER) 'sudo rsync -avzL bpan-org/ /var/www/bpan-org/ && sudo chown -R www-data /var/www/bpan-org/' 
+	ssh $(SERVER) 'sudo rsync -avzL bpan-org/ /var/www/bpan-org/ && sudo chown -R www-data /var/www/bpan-org/ && sudo /var/www/.rbenv/shims/god restart unicorn' 
 
 tail:
 	ssh $(SERVER) 'tail -f /var/www/bpan-org/logs/unicorn.log'
--- bpan.rb
+++ bpan.rb
@@ -3,14 +3,10 @@ require 'sinatra'
 require 'json'
 require 'logger'
 require 'fileutils'
+require 'erb'
 
 $stdout.sync = true
 logger = Logger.new $stdout, Logger::DEBUG
-
-INDEX_DIR = File.join(File.dirname(__FILE__), 'index')
-INDEX_BRANCH = 'index'
-AUTHORS_FILE = File.join(INDEX_DIR, 'authors.json')
-AUTHORS_FILEP = AUTHORS_FILE+'p'
 
 post '/star/?' do
   request.body.rewind  # in case someone already read it
@@ -31,32 +27,58 @@ end
 
 private
 
-def ensure_index_dir
-  return if Dir.exist?(INDEX_DIR)
-  FileUtils.mkdir_p(INDEX_DIR)
-  Git.clone('git@github.com:bpan-org/bpan-org.git', 'index', path: File.dirname(__FILE__), log: logger)
+def branch_dir branch
+  File.join(File.dirname(__FILE__), branch)
 end
 
-def repo
-  g = Git.open INDEX_DIR, log: logger
-  g.branch(INDEX_BRANCH).checkout
+INDEX_BRANCH = 'index'
+GH_PAGES_BRANCH = 'gh-pages'
+AUTHORS_FILE = File.join(branch_dir(INDEX_BRANCH), 'authors.json')
+AUTHORS_FILEP = AUTHORS_FILE+'p'
+HOMEPAGE_VIEW = ERB.new(File.read(File.join(File.dirname(__FILE__), 'views', 'index.html.erb')))
+HOMEPAGE_FILE = File.join(branch_dir(GH_PAGES_BRANCH), 'index.html')
+
+def ensure_dir branch
+  return if Dir.exist?(branch_dir branch)
+  FileUtils.mkdir_p(branch_dir branch)
+  Git.clone('git@github.com:bpan-org/bpan-org.git', branch, path: File.dirname(__FILE__), log: logger)
+end
+
+def repo(branch)
+  g = Git.open branch_dir(branch), log: logger
+  g.branch(branch).checkout
   g.config('user.name', 'BPAN index')
   g.config('user.email', 'index@bpan.org')
   return g
 end
 
-def ensure_index_updated
-  ensure_index_dir
-  repo.pull 'origin', INDEX_BRANCH
-  return repo
+def ensure_branch_updated branch
+  ensure_dir branch
+  repo(branch).pull 'origin', branch
+  return repo(branch)
 end
 
 def add_author author
-  git = ensure_index_updated
+  git = ensure_branch_updated INDEX_BRANCH
   authors = JSON.parse(File.read(AUTHORS_FILE))
   authors << format_author(author)
   authors.uniq! {|author| author["login"]}
   authors.sort! {|a,b| a["login"] <=> b["login"]}
+
+  # Regenerate homepage
+  git_gh = ensure_branch_updated GH_PAGES_BRANCH
+  File.open(HOMEPAGE_FILE, 'w') {|f|
+    f.write HOMEPAGE_VIEW.result(binding)
+  }
+  git_gh.add(File.basename(HOMEPAGE_FILE))
+  begin
+    git_gh.commit("Add author #{author['login'].inspect}")
+  rescue Git::GitExecuteError => e
+    raise e unless e.message =~ /nothing to commit/i
+  end
+  git_gh.push 'origin', GH_PAGES_BRANCH
+
+  # Regenerate json index
   json = authors.to_json
   File.open(AUTHORS_FILE, 'w') {|f|
     f.write json
@@ -68,8 +90,13 @@ def add_author author
   }
   git.add(File.basename(AUTHORS_FILE))
   git.add(File.basename(AUTHORS_FILEP))
-  git.commit("Add author '#{author}'")
+  begin
+    git.commit("Add author #{author['login'].inspect}")
+  rescue Git::GitExecuteError => e
+    raise e unless e.message =~ /nothing to commit/i
+  end
   git.push 'origin', INDEX_BRANCH
+
 end
 
 def format_author author
