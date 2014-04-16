b29ff1b717515eb2dd6fe32c24b5689935ed17a0
Ingy döt Net <ingy@ingy.net>
2014-04-16 16:46:19 +0800

    redo sample package.json

--- create-test-package-index.pl
+++ create-test-package-index.pl
@@ -19,13 +19,22 @@ for my $package (@packages) {
     my $dir = "../$package";
     my $meta = YAML::XS::LoadFile "$dir/Meta";
     my $name = $meta->{name} or die;
+    my $version = $meta->{version} or die;
 
     $meta->{release}{url} = "https://github.com/$owner/$package.git";
     $meta->{release}{sha} = `(cd $dir; git rev-parse HEAD)`;
     chomp $meta->{release}{sha};
 
-    $index{"$owner/$name"} = $meta;
-    $index{"$name"} = "$owner/$name";
+    $index{"$name/$owner/$version"} = $meta;
+    $index{"$name/$owner"} = [$version];
+    $index{"$name"} = "$name/$owner";
+
+    if ($version eq '0.0.1') {
+        $meta = Load Dump $meta;
+        $meta->{version} = $version = '0.0.2';
+        $index{"$name/$owner/$version"} = $meta;
+        unshift @{$index{"$name/$owner"}}, $version;
+    }
 }
 
 my $jxs = JSON::XS->new->ascii->pretty->allow_nonref->canonical;
