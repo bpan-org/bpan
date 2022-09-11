c67f1ac37dbf3413222bbc0a7530c4fc74c07d6a
Ingy döt Net <ingy@ingy.net>
2022-09-11 13:23:02 -0400

    Update to latest test-tap-bash

--- .bpan/lib/test-tap.bash
+++ .bpan/lib/test-tap.bash
@@ -1,33 +1,69 @@
-# test-more.bash - Complete TAP test framework for Bash
+# test-tap.bash - Complete TAP test framework for Bash
 #
 # Copyright (c) 2013-2022. Ingy döt Net.
 
-: "${BPAN_VERSION:?}"
-
-bpan:use prelude
-
-check-command-version bash 3.2 ||
-  die "test-more-bash requires bash 3.2+"
-
-test-more:main() {
+test-tap:main() {
   [[ ${BASH_SOURCE[0]} ]] ||
-    die "Error: test-more-bash must be run under Bash only"
-  _test_more__plan=0
-  _test_more__run=0
-  _test_more__failed=0
-  _test_more__pid=${BASHPID:-0}
+    test-tap:die "Error: test-tap-bash must be run under Bash only"
+  _test_tap__plan=0
+  _test_tap__run=0
+  _test_tap__failed=0
+  _test_tap__pid=${BASHPID:-0}
 
   if [[ $# -gt 0 ]]; then
     [[ $# -eq 2 ]] ||
-      die 'Usage: source test-more.bash tests <number>'
+      test-tap:die 'Usage: source test-tap.bash tests <number>'
     plan "$@"
   fi
 
-  trap test-more:END EXIT
+  trap test-tap:END EXIT
+}
+
+test-tap:label() (
+  label=$1
+  G=${got-}
+  W=${want-}
+  G=${G:0:40}
+  W=${W:0:40}
+  G=${G//$'\n'/␤}
+  W=${W//$'\n'/␤}
+
+  label=${label//\%G/$G}
+  label=${label//\%W/$W}
+
+  echo "$label"
+)
+
+try() {
+  cmd="$*"
+  set --
+  set +e
+  got=$(eval "$cmd" 2>&1)
+  rc=$?
+  set -e
+}
+
+has() {
+  local got=$1
+  local want=$2
+  local label
+  label=$(test-tap:label "$3")
+
+  if [[ $got == *"$want"* ]]; then
+    pass "$label"
+  else
+    fail "$label"
+    if [[ $got == *"$n"* ]]; then
+      got="'$n$got$n'"
+    else
+      got="'$got'"
+    fi
+    diag "Text '$want' not found in: $got"
+  fi
 }
 
 plan() {
-  test-more:_check-pid
+  test-tap:_check-pid
   if [[ $# -eq 2 && $1 == skip-all ]]; then
     printf "1..0 # SKIP %s\n" "$2"
     exit 0
@@ -39,62 +75,53 @@ plan() {
 
   if [[ $# -eq 1 ]]; then
     [[ $1 =~ ^-?[0-9]+$ ]] ||
-      die 'Plan must be a number'
+      test-tap:die 'Plan must be a number'
     [[ $1 -gt 0 ]] ||
-      die 'Plan must greater then 0'
-    _test_more__plan=$1
-    printf "1..%d\n" "$_test_more__plan"
+      test-tap:die 'Plan must be greater then 0'
+    _test_tap__plan=$1
+    printf "1..%d\n" "$_test_tap__plan"
   else
-    die 'Usage: plan <number>'
+    test-tap:die 'Usage: plan <number>'
   fi
 }
 
 pass() {
-  test-more:_check-pid
-  ((++_test_more__run))
-  local label=${1-}
-  if [[ $label ]]; then
-    echo "ok $_test_more__run - $label"
-  else
-    echo "ok $_test_more__run"
-  fi
+  test-tap:_check-pid
+  echo "ok $((++_test_tap__run))${1:+" - $1"}"
 }
 
-_test_more__CALL_STACK_LEVEL=1
+_test_tap__CALL_STACK_LEVEL=1
 fail() {
-  test-more:_check-pid
-  ((++_test_more__run))
-  IFS=' ' read -r -a c <<<"$(caller $_test_more__CALL_STACK_LEVEL)"
+  test-tap:_check-pid
+  ((++_test_tap__run))
+  IFS=' ' read -r -a c <<<"$(caller $_test_tap__CALL_STACK_LEVEL)"
   local file=${c[2]-}
   local line=${c[0]-}
   local label=${1-} callback=${2-}
-  if [[ $label ]]; then
-    echo "not ok $_test_more__run - $label"
-  else
-    echo "not ok $_test_more__run"
-  fi
+  echo "not ok $_test_tap__run${label:+" - $label"}"
   label=${label:+"'$label'\n#   at $file line $line."}
   label=${label:-"at $file line $line."}
   echo -e "#   Failed test $label" >&2
 
   [[ $callback ]] && $callback
 
-  local rc=${TEST_MORE_BAIL_ON_FAIL:-0}
+  local rc=${TEST_TAP_BAIL_ON_FAIL:-0}
   [[ $rc -eq 0 ]] || exit "$rc"
 }
 
 is() {
-  local got=$1 want=$2 label=${3-}
+  local got=$1 want=$2 label
+  label=$(test-tap:label "$3")
   if [[ $got == "$want" ]]; then
     pass "$label"
   else
-    fail "$label" test-more:is-fail
+    fail "$label" test-tap:is-fail
   fi
 }
 
-test-more:is-fail() {
-  local _test_more__CALL_STACK_LEVEL=
-  _test_more__CALL_STACK_LEVEL=$(( _test_more__CALL_STACK_LEVEL + 1 ))
+test-tap:is-fail() {
+  local _test_tap__CALL_STACK_LEVEL=
+  _test_tap__CALL_STACK_LEVEL=$(( _test_tap__CALL_STACK_LEVEL + 1 ))
   if [[ $want =~ $'\n' ]]; then
     echo "$got" > /tmp/got-$$
     echo "$want" > /tmp/want-$$
@@ -109,17 +136,17 @@ test-more:is-fail() {
 }
 
 isnt() {
-  local _test_more__CALL_STACK_LEVEL=
-  _test_more__CALL_STACK_LEVEL=$(( _test_more__CALL_STACK_LEVEL + 1 ))
+  local _test_tap__CALL_STACK_LEVEL=
+  _test_tap__CALL_STACK_LEVEL=$(( _test_tap__CALL_STACK_LEVEL + 1 ))
   local got=$1 dontwant=$2 label=${3-}
   if [[ $got != "$dontwant" ]]; then
     pass "$label"
   else
-    fail "$label" test-more:isnt-fail
+    fail "$label" test-tap:isnt-fail
   fi
 }
 
-test-more:isnt-fail() {
+test-tap:isnt-fail() {
     diag "\
       got: '$got'
    expected: anything else"
@@ -162,11 +189,11 @@ like() {
   if [[ $got =~ $regex ]]; then
     pass "$label"
   else
-    fail "$label" test-more:like-fail
+    fail "$label" test-tap:like-fail
   fi
 }
 
-test-more:like-fail() {
+test-tap:like-fail() {
     diag "Got: '$got'"
 }
 
@@ -175,11 +202,11 @@ unlike() {
   if [[ ! $got =~ $regex ]]; then
     pass "$label"
   else
-    fail "$label" test-more:unlike-fail
+    fail "$label" test-tap:unlike-fail
   fi
 }
 
-test-more:unlike-fail() {
+test-tap:unlike-fail() {
     diag "Got: '$got'"
 }
 
@@ -201,77 +228,81 @@ skip-all() {
 }
 
 done-testing() {
-  test-more:_check-pid
-  _test_more__plan=$_test_more__run
-  echo 1.."${1:-$_test_more__run}"
+  test-tap:_check-pid
+  _test_tap__plan=$_test_tap__run
+  echo 1.."${1:-$_test_tap__run}"
 }
 
 diag() {
-  test-more:_check-pid
+  test-tap:_check-pid
   local msg=$*
   msg="# ${msg//$'\n'/$'\n'\# }"
   echo "$msg" >&2
 }
 
 note() {
-  test-more:_check-pid
+  test-tap:_check-pid
   local msg=$*
   msg="# ${msg//$'\n'/$'\n'\# }"
   echo "$msg"
 }
 
 BAIL-OUT() {
-  test-more:_check-pid
-  _test_more__bail_msg=$*
-  : "${_test_more__bail_msg:=No reason given.}"
+  test-tap:_check-pid
+  _test_tap__bail_msg=$*
+  : "${_test_tap__bail_msg:=No reason given.}"
   exit 255
 }
 
 BAIL-ON-FAIL() {
-  test-more:_check-pid
-  TEST_MORE_BAIL_ON_FAIL=1
+  test-tap:_check-pid
+  TEST_TAP_BAIL_ON_FAIL=1
 }
 
-test-more:END() {
+test-tap:END() {
   local rc=$?
-  test-more:_check-pid
+  test-tap:_check-pid
   if [[ $rc -ne 0 ]]; then
-    if [[ ${_test_more__bail_msg-} ]] ||
-       [[ ${TEST_MORE_BAIL_ON_FAIL-} ]]; then
-      local bail=${_test_more__bail_msg:-"Bailing out on status=$rc"}
+    if [[ ${_test_tap__bail_msg-} ]] ||
+       [[ ${TEST_TAP_BAIL_ON_FAIL-} ]]; then
+      local bail=${_test_tap__bail_msg:-"Bailing out on status=$rc"}
       echo "Bail out!  $bail"
       exit $rc
     fi
   fi
 
-  if [[ $_test_more__plan -eq 0 ]]; then
-    if [[ $_test_more__run -gt 0 ]]; then
+  if [[ $_test_tap__plan -eq 0 ]]; then
+    if [[ $_test_tap__run -gt 0 ]]; then
       echo "# Tests were run but no plan was declared." >&2
     fi
   else
-    if [[ $_test_more__run -eq 0 ]]; then
+    if [[ $_test_tap__run -eq 0 ]]; then
       echo "# No tests run!" >&2
-    elif [[ $_test_more__run -ne $_test_more__plan ]]; then
-      local msg="# Looks like you planned $_test_more__plan tests but ran $_test_more__run."
-      [[ $_test_more__plan -eq 1 ]] && msg=${msg/tests/test}
+    elif [[ $_test_tap__run -ne $_test_tap__plan ]]; then
+      local msg="# Looks like you planned $_test_tap__plan tests but ran $_test_tap__run."
+      [[ $_test_tap__plan -eq 1 ]] && msg=${msg/tests/test}
       echo "$msg" >&2
     fi
   fi
   local exit_code=0
-  if [[ $_test_more__failed -gt 0 ]]; then
-    exit_code=$_test_more__failed
+  if [[ $_test_tap__failed -gt 0 ]]; then
+    exit_code=$_test_tap__failed
     [[ $exit_code -gt 254 ]] && exit_code=254
-    local msg="# Looks like you failed $_test_more__failed tests of $_test_more__run run."
-    [[ $_test_more__failed -eq 1 ]] && msg=${msg/tests/test}
+    local msg="# Looks like you failed $_test_tap__failed tests of $_test_tap__run run."
+    [[ $_test_tap__failed -eq 1 ]] && msg=${msg/tests/test}
     echo "$msg" >&2
   fi
   exit $exit_code
 }
 
-test-more:_check-pid() {
-  if [[ ${BASHPID:-0} -ne ${_test_more__pid:-0} ]]; then
-    die "Error: Called test-more method from a subprocess" 3
+test-tap:_check-pid() {
+  if [[ ${BASHPID:-0} -ne ${_test_tap__pid:-0} ]]; then
+    test-tap:die "Error: Called test-tap method from a subprocess" 3
   fi
 }
 
-test-more:main "$@"
+test-tap:die() {
+  echo "$1" >&2
+}
+
+test-tap:main "$@"
