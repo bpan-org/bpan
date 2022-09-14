0a3ccbed6ac0020b068b5e222dc5b864983a9d78
Ingy döt Net <ingy@ingy.net>
2022-09-14 09:30:30 -0400

    Add variable expansion to config

--- .bpan/lib/config.bash
+++ .bpan/lib/config.bash
@@ -2,11 +2,20 @@ config_file=''
 config_files=()
 
 config:get() (
-  [[ $config_file ]] && config_files=( "$config_file" )
   [[ $# -gt 0 ]] ||
     config:die "config:get requires 1 or more keys"
+  [[ $config_file ]] && config_files=( "$config_file" )
+  config=$(cat "${config_files[@]?}")
   for key; do
-    if value=$(git config --file <(cat "${config_files[@]?}") "$key"); then
+    if value=$(git config -f- "$key" <<<"$config"); then
+      i=0
+      while [[ $value =~ \$([.a-z0-9]+) ]]; do
+        repl=$(git config -f- "${BASH_REMATCH[1]}" <<<"$config") ||
+          config:die "Can't expand config value '$key'"
+        value=${value//\$${BASH_REMATCH[1]}/$repl}
+        [[ $((i++)) -lt 10 ]] ||
+          config:die "Possible infinite recursion in config value '$key'"
+      done
       echo "$value"
       return
     fi
