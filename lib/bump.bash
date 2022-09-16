4c1363ebc0d1175b2aa365188b324d1d27a63549
Ingy döt Net <ingy@ingy.net>
2022-09-16 15:19:04 -0400

    Allow 'bpan bump' to update index (ingydotnet only)

--- lib/bump.bash
+++ lib/bump.bash
@@ -1,8 +1,16 @@
 bump:options() (
-  echo "p,push   Push changes upstream"
+  echo "p,push      Push changes upstream"
+  user=$(bpan config bpan.user.github || true)
+  if [[ $user == ingydotnet ]]; then
+    echo "r,release   Push and update bpan-index"
+  fi
 )
 
 bump:main() (
+  if ${option_release:-false}; then
+    option_push=true
+  fi
+
   git:in-top-dir ||
     error "'$app $cmd' must be at repo toplevel"
   git:is-clean ||
@@ -30,9 +38,6 @@ bump:main() (
       done
   )
 
-  [[ $list ]] ||
-    error "No changes commited since version '$version1'"
-
   grep -q -i '^wip\>' <<<"$list" &&
     error "Can't '$app $cmd' with WIP commits"
 
@@ -40,6 +45,17 @@ bump:main() (
     branch=$(git:branch-name)
     [[ $branch ]] ||
       error "Can't push. Not checked out to a branch."
+    # TODO make 'main' be a config setting
+    [[ $branch == main ]] ||
+      error "Can't push. Current branch is not 'main'"
+  fi
+
+  if ! [[ $list ]]; then
+    if $option_push; then
+      bump:push
+      return
+    fi
+    error "No changes commited since version '$version1'"
   fi
 
   say -y "Running 'bpan update'"
@@ -70,14 +86,13 @@ $list"
   say -y "Updated 'Meta' file"
 
   for bin in $(shopt -s nullglob; echo bin/*); do
-    temp=$(mktemp)
+    temp=$(+mktemp)
     perl -pe "s/^VERSION=\d+\.\d+\.\d+(.*)/VERSION=$version2\$1/" \
       < "$bin" > "$temp"
+    chmod =rwx "$temp"
     if +is-file-diff "$bin" "$temp"; then
       mv "$temp" "$bin"
       say -y "Updated VERSION=... in '$bin'"
-    else
-      rm 
     fi
   done
 
@@ -90,8 +105,7 @@ $list"
   say -y "Commit tagged as '$version2'"
 
   if $option_push; then
-    git push -q --tag origin "$branch"
-    say -y "Pushed to origin '$branch'"
+    bump:push
   fi
 
   say -g "Version bump complete"
@@ -100,4 +114,27 @@ $list"
 
   say -y Commits:
   git log --pretty=oneline "$version1^..$version2"
+)
+
+bump:push() (
+  git push -q --tag origin "$branch"
+  say -y "Pushed to origin '$branch'"
+
+  # A temporary hack for ingydotnet only to update bpan-index after a
+  # 'bpan bump -p'
+  if ${option_release:-false}; then
+    name=$(bpan config bpan.name) ||
+      die "Can't get config value 'bpan.name'"
+    [[ $name != bpan ]] ||
+      error "Can't update index for 'bpan' itself"
+
+    index_repo=~/src/+bpan/bpan-index
+
+    echo
+    say -y "Updating $name=$version2 in bpan-org/bpan-index index.ini"
+
+    make -C "$index_repo" update NAME="$name"
+
+    say -g "BPAN index updated"
+  fi
 )
