30a7e30f99b40b2a21128e04614fba0a24b35111
Ingy döt Net <ingy@ingy.net>
2022-10-14 15:29:06 -0700

    Make bpan.bash smaller

--- lib/bpan.bash
+++ lib/bpan.bash
@@ -3,10 +3,7 @@
 
 # 'die' is so common we define a very basic one here.
 # The 'bashplus' library defines a more full featured one.
-die() {
-  printf '%s\n' "$@" >&2
-  exit 1
-}
+die() { printf '%s\n' "$@" >&2; exit 1; }
 
 # Assert that Bash in version 3.2 or higher:
 ( shopt -s compat31 2>/dev/null ) ||
@@ -45,59 +42,41 @@ bpan:main() {
       *) die "Unknown argument '$arg' for '${BASH_SOURCE[0]}'";;
     esac
   done
-
-  # Set the BPAN_INSTALL variable (unless already set):
-  if [[ ${BPAN_INSTALL-} ]]; then
-    [[ -d ${BPAN_INSTALL} ]] ||
-      die "BPAN_INSTALL='$BPAN_INSTALL' is not a directory"
-  else
-    if [[ ${BPAN_ROOT-} && -f $BPAN_ROOT/lib/bpan.bash ]]; then
-      BPAN_INSTALL=$BPAN_ROOT/local
-      [[ -d $BPAN_INSTALL/lib ]] ||
-        mkdir -p "$BPAN_INSTALL/lib"
-    fi
-  fi
-
-  # BPAN_PATH is an array of directories for 'bpan:source' to find libraries:
-  BPAN_PATH=()
-
-  # Determine the package project/install's root directory:
-  local project_root
-  project_root=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)
-  project_root=${project_root#.bpan/}
-
-  # Add the project's BPAN dependency directory:
-  if [[ -d $project_root/.bpan/lib ]]; then
-    BPAN_PATH+=("$project_root/.bpan/lib")
-  fi
-
-  # Add BPAN's install directory:
-  if [[ ${BPAN_INSTALL-} && -d ${BPAN_INSTALL}/lib ]]; then
-    BPAN_PATH+=("$BPAN_INSTALL}/lib")
-  fi
 }
 
-# source libraries found in BPAN_PATH array.
-#
-# TODO support calling forms:
-# * bpan:source foo
-# * bpan:source foo/bar
-# * bpan:source foo/bar arg1 arg2
-# * bpan:source foo/bar=1.2.3               # TODO
-# * bpan:source foo/bar=1.2.3+              # TODO
-# * bpan:source foo/bar=1.2.3+ arg1 arg2    # TODO
+# Try to source a bash library from one of:
+# * A directory in BPAN_PATH
+# * Local .bpan/lib/
+# * BPAN_INSTALL/lib/
+# * BPAN_ROOT/local/lib/
 bpan:source() {
   [[ $# -gt 0 ]] ||
     die "Usage: bpan:source <bpan-library-name> [<arg>...]"
   local name=$1; shift
   local dir
 
-  for dir in "${BPAN_PATH[@]}"; do
+  # shellcheck disable=2086
+  for dir in $(IFS=:; echo ${BPAN_PATH-}); do
     if [[ -f $dir/$name.bash ]]; then
       source "$dir/$name.bash" "$@"
       return
     fi
   done
+
+  dir=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)
+  dir=${dir#.bpan/}
+  if [[ -f $dir/.bpan/lib/$name.bash ]]; then
+    source "$dir/.bpan/lib/$name.bash" "$@"
+    return
+  fi
+
+  if [[ ${BPAN_INSTALL-} ]] &&
+     [[ -d ${BPAN_INSTALL}/lib/$name.bash ]]; then
+    source "$dir/.bpan/lib/$name.bash" "$@"
+  elif [[ ${BPAN_ROOT-} ]] &&
+       [[ -d ${BPAN_ROOT}/local/lib/$name.bash ]]; then
+    source "${BPAN_ROOT}/local/lib/$name.bash" "$@"
+  fi
 }
 
 bpan:main "$@"
