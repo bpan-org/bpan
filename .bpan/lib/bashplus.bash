188ce81ce8a38740ea9a8d048c51b9dc0f5d68f8
Ingy döt Net <ingy@ingy.net>
2022-11-02 15:04:18 -0700

    Update bashplus with stack-trace support

--- .bpan/lib/bashplus.bash
+++ .bpan/lib/bashplus.bash
@@ -4,7 +4,7 @@
 # * Many are improved versions of existing builtins/commands.
 
 
-bashplus:version() ( echo '0.1.44' )
+bashplus:version() ( echo '0.1.45' )
 
 bashplus:main() {
   bashplus_lib=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
--- .bpan/lib/bashplus/err.bash
+++ .bpan/lib/bashplus/err.bash
@@ -3,20 +3,23 @@
 # It also supports:
 # * --level for stack trace level adjustment
 # * --red for error messages (to tty) in color red
-# * TODO --line to show line numbers
-# * TODO --plain to not show line numbers
-# * TODO --stack for full stack trace
+# * --stack for full stack trace
+# * --line to show line numbers
+# * --plain to not show line numbers
 #
 # NOTE: 'die', 'errpr' and 'warn' are the only bashplus functions not
 # starting with a '+' character.
 
 die() {
   set +x
-  local arg args=() level=0 R='' Z=''
+  local arg args=() level=1 R='' Z=''
+  local line=false plain=false stack=false
 
   for arg; do
     if [[ $arg =~ ^--level=([0-9]+)$ ]]; then
-      level=${BASH_REMATCH[1]}
+      level=$(( level + BASH_REMATCH[1] ))
+    elif [[ $arg =~ ^--(line|plain|stack)$ ]]; then
+      printf -v "${BASH_REMATCH[1]}" true
     elif [[ $arg == --red ]]; then
       if [[ -t 2 ]]; then
         R='\e[31m'
@@ -36,31 +39,73 @@ die() {
     echo -en "$Z"
   ) >&2
 
-  # If last msg line does not end with newline, then print line number:
-  if [[ ${!#} != *$'\n' ]]; then
-    local caller
-    IFS=' ' read -r -a caller <<< "$(caller "$level")"
-    if (( ${#caller[@]} == 2 )); then
-      printf ' at line %d of %s\n' "${caller[@]}" >&2
-    else
-      printf ' at line %d in %s of %s\n' "${caller[@]}" >&2
+  (
+    if $stack || [[ ${BASHPLUS_DEBUG_STACK-} ]]; then
+      +stack-trace $level
+    elif $plain; then
+      :
+    elif $line || [[ ${!#} != *$'\n' ]]; then
+      +stack-trace $level 1
     fi
-  fi
+  ) >&2
 
   exit 1
 }
 
+function +stack-trace() (
+  set +x
+
+  max=15
+  if [[ ${BASHPLUS_DEBUG_STACK-} =~ ^[0-9]+$ ]] &&
+    (( BASHPLUS_DEBUG_STACK > 1 ))
+  then
+    max=$BASHPLUS_DEBUG_STACK
+  fi
+  level=$1 lines=${2:-$max} count=1
+
+  while IFS=' ' read -r -a caller <<< "$(caller "$level")"; do
+    case ${#caller[*]} in
+      2)
+        printf ' at line %-3d of %s\n' "${caller[@]}"
+        line=${caller[0]}
+        file=${caller[1]}
+        ;;
+      3)
+        printf ' at line %-3d in %-15s of %s\n' "${caller[@]}"
+        line=${caller[0]}
+        file=${caller[2]}
+        ;;
+      *) break ;;
+    esac
+
+    if (( lines > 1 )); then
+      text=$(tail -n+"$line" "$file" | head -n1)
+      text=${text#"${text%%[![:space:]]*}"}
+      echo "    ${text:0:75}"
+    fi
+
+    : $(( level++ ))
+    if (( count++ >= lines )); then
+      echo ' ...'
+      break
+    fi
+  done
+)
+
 # Red die with 'Error: ' prefix
 error() {
   set +x
+
   local msg
+
   if [[ ${app-} && $app =~ ^[-a-z0-9]+$ ]]; then
     msg="[$app] Error: $1"
   else
     msg="Error: $1"
   fi
   shift
-  die --red --level=1 "$msg" "$@"
+
+  die --plain --red --level=1 "$msg" "$@"
 }
 
 # Allow multiple traps to be performed.
