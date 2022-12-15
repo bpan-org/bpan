1da78152ba7212cfcf4e12b9ea2201834706f0c9
Ingy döt Net <ingy@ingy.net>
2022-12-15 09:59:48 -0800

    Update bashplus/time.bash for fixed timestamp

--- .bpan/lib/bashplus.bash
+++ .bpan/lib/bashplus.bash
@@ -4,7 +4,7 @@
 # * Many are improved versions of existing builtins/commands.
 
 
-bashplus:version() ( echo '0.1.61' )
+bashplus:version() ( echo '0.1.62' )
 
 bashplus:main() {
   local arg
--- .bpan/lib/bashplus/time.bash
+++ .bpan/lib/bashplus/time.bash
@@ -7,9 +7,9 @@ else
 fi
 
 if ( shopt -s compat41 2>/dev/null ); then
-  +time:stamp() { printf '%(%Y-%m-%d-%H-%M-%S)T\n' -1; }
+  +time:stamp() { printf '%(%Y-%m-%dT%H:%M:%SZ)T\n' -1; }
   +time:ymd() { printf '%(%Y-%m-%d)T\n' -1; }
 else
-  +time:stamp() { date '+%Y-%m-%d-%H-%M-%S'; }
+  +time:stamp() { date '+%Y-%m-%dT%H:%M:%SZ'; }
   +time:ymd() { date '+%Y-%m-%d'; }
 fi
